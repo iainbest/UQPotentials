7a0b91d3319c6a32fea7145ff9e9c6ace1bf6deb
Iain Best <iain.best@hotmail.com>
2021-11-09 15:32:25 +0000

    Fixed first unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using UQPotentials
 using Test
+using Distributions
 
 @testset "UQPotentials.jl" begin
     # Write your tests here.
 
-    @test log_param_priors([0.0],[Normal(0.0,1.0)]) = -0.9189 atol=0.0001
+    @test log_param_priors([0.0],[Normal(0.0,1.0)]) ≈ -0.9189 atol=0.0001
 
 end
