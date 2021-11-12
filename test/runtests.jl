9fe3746c8f30e918dafda24dcff37b6f303e8035
Iain Best <iain.best@hotmail.com>
2021-11-12 12:17:04 +0000

    Update test skeleton

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,15 @@ using Distributions
 @testset "UQPotentials.jl" begin
     # Write your tests here.
 
+    # BayesianFunctions.jl tests
     @test log_param_priors([0.0],[Normal(0.0,1.0)]) ≈ -0.9189 atol=0.0001
+
+    # ExtraPlotFunctions.jl tests
+
+    # GPConstruction.jl tests
+
+    # MCMCFunctions.jl tests
+
+    # UtilityFunctions.jl tests
 
 end
