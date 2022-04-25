f5396c2b81fa0b33b1e560e054b68f32f194e407
Iain Best <iain.best@hotmail.com>
2022-04-25 11:04:54 +0100

    Add extra utility functions

--- src/UtilityFunctions.jl
+++ src/UtilityFunctions.jl
@@ -3,6 +3,8 @@
 # using Statistics
 
 export characterise_uncertainty, vecvec_to_matrix
+export virial_matrix_to_vector
+export unpack_vecvec_of_3tuples
 
 function characterise_uncertainty(QoI_list,ref_value=nothing,ref_stdev=nothing)
     QoI_mean = mean(QoI_list)
@@ -35,6 +37,63 @@ function vecvec_to_matrix(vecvec)
         end
     end
     return my_array
+end
+
+"""
+    virial_matrix_to_vector(matrix)
+
+Take 3x3 virial stress matrix, return corresponding voigt vector. In matrix notation (row,column), the 
+elements 11,22,33,23,13,12 are contained in the vector, in that order.
+"""
+function virial_matrix_to_vector(matrix::AbstractArray{Float64})
+    
+    @assert size(matrix) == (3,3)
+    
+    m_vector = vec(transpose(matrix))
+    
+    voigt_indices = [1,5,9,6,3,2]
+    
+    voigt_vector = m_vector[voigt_indices]
+    
+    return voigt_vector
+end
+
+"""
+    unpack_vecvec_of_3tuples(vecvec_3tuples)
+
+Unpack vector of vector of tuples (with each tuple having 3 entries) in 3 vector of vectors of individual 
+entries
+
+### Arguments
+
+- `vecvec_3tuples`
+"""
+function unpack_vecvec_of_3tuples(vecvec_3tuples)
+
+    vecvec_tuple1 = Vector{Vector{Float64}}(undef,length(vecvec_3tuples))
+    vecvec_tuple2 = Vector{Vector{Float64}}(undef,length(vecvec_3tuples))
+    vecvec_tuple3 = Vector{Vector{Float64}}(undef,length(vecvec_3tuples))
+
+
+    for (count_,tuple) in enumerate(vecvec_3tuples) 
+        vec_tuple1 = zeros(length(tuple))
+        vec_tuple2 = zeros(length(tuple))
+        vec_tuple3 = zeros(length(tuple))
+        for (count,i) in enumerate(tuple)
+
+            vec_tuple1[count] = i[1]
+            vec_tuple2[count] = i[2]
+            vec_tuple3[count] = i[3]
+
+        end
+
+        vecvec_tuple1[count_] = vec_tuple1
+        vecvec_tuple2[count_] = vec_tuple2
+        vecvec_tuple3[count_] = vec_tuple3
+
+    end
+    
+    return vecvec_tuple1,vecvec_tuple2,vecvec_tuple3
 end
 
 # end
